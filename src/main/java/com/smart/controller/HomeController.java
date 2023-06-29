fe2d170be2d76f3ddbad2edd2656f59dd1666020
Suraj Hanvate <surajhanvate999@gmail.com>
2023-06-29 18:31:15 +0530

    user registration with success/error message

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,27 +1,70 @@
 package com.smart.controller;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("title","Home - Smart Contact Manager");
+        model.addAttribute("title", "Home - Smart Contact Manager");
         return "home";
     }
 
     @RequestMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title","About - Smart Contact Manager");
+        model.addAttribute("title", "About - Smart Contact Manager");
         return "about";
     }
 
     @RequestMapping("/signup")
     public String signup(Model model) {
         model.addAttribute("title", "SignUp - Smart Contact Manager");
+        model.addAttribute("user", new User());
         return "signup";
+    }
+
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String userRegister(@ModelAttribute("user") User user, Model model, @RequestParam(value = "terms", defaultValue = "false") boolean terms, HttpSession session) {
+        try {
+            if (!terms) {
+                System.out.println("Please agree to the Terms and Conditions!");
+                throw new Exception("Please agree to the Terms and Conditions!");
+            }
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            user.setImageUrl("default.png");
+
+            System.out.println("Terms and Conditions " + terms);
+            System.out.println("USER " + user);
+
+            User savedUser = userRepository.save(user);
+
+            model.addAttribute("user", new User());
+
+            session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
+
+            return "signup";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message(e.getMessage(), "alert-danger"));
+
+            return "signup";
+        }
     }
 }
