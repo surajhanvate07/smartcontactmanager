72bcc354687366c254691178ebf3f8f31d7ef80e
Suraj Hanvate <surajhanvate999@gmail.com>
2023-06-29 09:16:19 +0530

    Designing home page, added banner

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home(Model model) {
         model.addAttribute("title","Home - Smart Contact Manager");
         return "home";
@@ -15,7 +15,13 @@ public class HomeController {
 
     @RequestMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title","About Page");
+        model.addAttribute("title","About - Smart Contact Manager");
         return "about";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("title", "SignUp - Smart Contact Manager");
+        return "signup";
     }
 }
